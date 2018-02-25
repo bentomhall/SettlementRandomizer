92346ad4c5e0919efd01b2cc3348358963ae4ef8
Ben Hall <bentomhall@gmail.com>
2018-02-25 18:55:57 -0500

    Add project files.

---
+++ SettlementRandomizer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SettlementRandomizer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
